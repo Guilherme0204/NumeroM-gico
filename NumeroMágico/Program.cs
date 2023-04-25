d7d093d1f78ebf8e70962359ff93acc218d9b9f2
Guilherme0204 <gui2605costa@gmail.com>
2023-04-25 09:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ NumeroMágico/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroMágico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, div, mod, soma;
+            
+
+          for (num = 1000; num <= 9999; num++)
+            {
+                    div = num / 100;
+                    mod = num % 100;
+                    soma = div + mod;
+                    soma = soma * soma;
+                    if (soma == num)
+                    {
+                        Console.WriteLine($"{soma} é um número mágico ");
+                    }
+               
+            } 
+            Console.ReadKey();
+            
+
+        }
+    }
+}
